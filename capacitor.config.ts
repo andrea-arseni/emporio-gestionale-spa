fea6a49cfe5146f02a33e4f324bd57bf4c631594
Andrea Arseni <andrea.arseni87@gmail.com>
2022-12-30 13:48:58 +0100

    Restyling of HouseCard component and Bottstrap Icons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-    appId: "it.emporiocase.nativeapp",
+    appId: "it.emporiocase.native",
     appName: "Emporio Case",
     webDir: "build",
     bundledWebRuntime: false,
